42c3a99f3ec842fccc1145e7b9168a6d35cf7db1
David Hargitai <david@leadforward.hu>
2021-03-12 08:32:28 +0100

    wip attributes refactor

--- practice-4/src/user.ts
+++ practice-4/src/user.ts
@@ -2,47 +2,61 @@ import axios, { AxiosResponse } from "axios";
 
 type Callback = () => void;
 
+// type UserProps = {
+//   [key: string]: string | number | boolean;
+// };
+type UserProps = {
+  id?: number;
+  name?: string;
+  age?: number;
+};
+
+class Attributes<T> {
+  constructor(public data: T) {}
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set<K extends keyof T>(key: K, value: T[K]): void {
+    this.data[key] = value;
+    // this.dispatch("edit");
+  }
+}
+
 export class User {
   events: { [key: string]: Callback[] } = {};
+  attributes: Attributes<UserProps>;
 
-  constructor(
-    public data: {
-      [key: string]: string | number | boolean;
-    }
-  ) {}
+  constructor(attributes: UserProps) {
+    this.attributes = new Attributes<UserProps>(attributes);
+  }
 
   save(): void {
-    if (typeof this.data.id === "number") {
+    if (typeof this.attributes.get("id") === "number") {
       axios
-        .put(`http://localhost:3000/users/${this.data.id}`, this.data)
+        .put(
+          `http://localhost:3000/users/${this.attributes.get("id")}`,
+          this.attributes
+        )
         .then(() => this.dispatch("save"));
     } else {
       axios
-        .post("http://localhost:3000/users", this.data)
+        .post("http://localhost:3000/users", this.attributes)
         .then(() => this.dispatch("save"));
     }
   }
 
   async fetch(): Promise<AxiosResponse | void> {
-    if (typeof this.data.id !== "number") {
+    if (typeof this.attributes.get("id") !== "number") {
       throw new Error("Missing id");
     }
 
     const response = await axios.get(
-      `http://localhost:3000/users/${this.data.id}`
+      `http://localhost:3000/users/${this.attributes.get("id")}`
     );
-    this.data = { ...this.data, ...response.data };
+    this.attributes = { ...this.attributes, ...response.data };
     this.dispatch("fetch");
-  }
-
-  get(key: string): string | number | boolean {
-    return this.data[key];
-  }
-
-  set(key: string, value: string | number | boolean): User {
-    this.data[key] = value;
-    this.dispatch("edit");
-    return this;
   }
 
   subscribe(event: string, callback: Callback): void {
